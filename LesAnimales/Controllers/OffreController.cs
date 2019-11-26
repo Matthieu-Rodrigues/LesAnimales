61544309952fe3bc98628b9f3064d2c23f2a7c61
Lilian Berna <arphax33@gmail.com>
2019-11-26 16:34:16 +0100

    First commit

---
+++ LesAnimales/Controllers/OffreController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LesAnimales.Controllers
+{
+    public class OffreController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail(int id)
+        {
+            return View();
+        }
+    }
+}
