b4f339394380f5387eb39cfee1ad4f816f18fb03
Lilian Berna <arphax33@gmail.com>
2019-11-27 09:05:00 +0100

    commit for merge with master

--- LesAnimales/Controllers/OffreController.cs
+++ LesAnimales/Controllers/OffreController.cs
@@ -11,87 +11,84 @@ namespace LesAnimales.Controllers
     public class OffreController : Controller
     {
         // GET: Offre
-//        public ActionResult Index()
-//        {
-//            return View();
-//        }
+        public ActionResult Index()
+        {
+            return View();
+        }
 
         // GET: Offre/Details/5
         public ActionResult Details(int id)
         {
-            var offre = new Offre(1, "oui", "description oui",
-                "https://www.mypokecard.com/fr/Galerie/my/galery/YsjyBLEWOLo.jpg", 10.20);
-            ViewBag.Message = offre;
             return View();
         }
 
-//        // GET: Offre/Create
-//        public ActionResult Create()
-//        {
-//            return View();
-//        }
-//
-//        // POST: Offre/Create
-//        [HttpPost]
-//        [ValidateAntiForgeryToken]
-//        public ActionResult Create(IFormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add insert logic here
-//
-//                return RedirectToAction(nameof(Index));
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
-//
-//        // GET: Offre/Edit/5
-//        public ActionResult Edit(int id)
-//        {
-//            return View();
-//        }
-//
-//        // POST: Offre/Edit/5
-//        [HttpPost]
-//        [ValidateAntiForgeryToken]
-//        public ActionResult Edit(int id, IFormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add update logic here
-//
-//                return RedirectToAction(nameof(Index));
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
-//
-//        // GET: Offre/Delete/5
-//        public ActionResult Delete(int id)
-//        {
-//            return View();
-//        }
-//
-//        // POST: Offre/Delete/5
-//        [HttpPost]
-//        [ValidateAntiForgeryToken]
-//        public ActionResult Delete(int id, IFormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add delete logic here
-//
-//                return RedirectToAction(nameof(Index));
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
+        // GET: Offre/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Offre/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Offre/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Offre/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Offre/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Offre/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
 }
